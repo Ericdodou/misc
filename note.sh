3973fc654d71e545a680a8a10aca23d85761ab36
Ericdodou <ericjohnsonq@gmail.com>
2016-03-01 01:59:07 -0800

    upload install llvm

--- note.sh
+++ note.sh
@@ -4,15 +4,12 @@ sudo bash -c "cat >> /etc/apt/sources.list" << LLVMAPT
 # LLVM
 deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty main
 deb-src http://llvm.org/apt/trusty/ llvm-toolchain-trusty main
-# 3.5 
-deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.5 main
-deb-src http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.5 main
-# 3.6 
-deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.6 main
-deb-src http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.6 main
+# 3.7
+deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.7 main
+deb-src http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.7 main
 LLVMAPT
 
 wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add -
 
 sudo apt-get update -y
-sudo apt-get install -y clang-3.6 lldb-3.6 libc++-dev libc++abi-dev
+sudo apt-get install -y clang-3.7 lldb-3.7 libc++-dev libc++abi-dev
